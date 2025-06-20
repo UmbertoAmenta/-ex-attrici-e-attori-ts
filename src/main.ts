9eaceafc32ebb02a5db8a0f4fa0939e3bdaa9a34
Umberto Amenta <umberto.amenta@hotmail.it>
2025-06-20 12:19:50 +0200

    feat: bonus 1, 2, 3

--- src/main.ts
+++ src/main.ts
@@ -22,21 +22,23 @@ type Person = {
 //  awards: una stringa
 //  nationality: una stringa tra un insieme definito di valori.
 //      Le nazionalità accettate sono: American, British, Australian, Israeli-American, South African, French, Indian, Israeli, Spanish, South Korean, Chinese.
+type Nationality_p =
+  | "American"
+  | "British"
+  | "Australian"
+  | "Israeli-American"
+  | "South African"
+  | "French"
+  | "Indian"
+  | "Israeli"
+  | "Spanish"
+  | "South Korean"
+  | "Chinese";
+
 type Actress = Person & {
   most_famous_movies: [string, string, string];
   awards: string;
-  nazionality:
-    | "American"
-    | "British"
-    | "Australian"
-    | "Israeli-American"
-    | "South African"
-    | "French"
-    | "Indian"
-    | "Israeli"
-    | "Spanish"
-    | "South Korean"
-    | "Chinese";
+  nazionality: Nationality_p;
 };
 
 // 📌 Milestone 3
@@ -58,7 +60,7 @@ const actressNationalities = [
   "Chinese",
 ];
 
-function isActress(data: unknown): data is Actress {
+function isPerson(data: unknown): data is Person {
   if (
     // controlli generici
     data &&
@@ -76,7 +78,16 @@ function isActress(data: unknown): data is Actress {
     "biography" in data &&
     typeof data.biography === "string" &&
     "image" in data &&
-    typeof data.image === "string" &&
+    typeof data.image === "string"
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function isActress(data: unknown): data is Actress {
+  if (
+    isPerson(data) &&
     // Actress
     "most_famous_movies" in data &&
     data.most_famous_movies instanceof Array &&
@@ -167,11 +178,18 @@ async function getActresses(ids: number[]): Promise<(Actress | null)[]> {
 // Utilizza gli Utility Types:
 //  Omit: per creare un'attrice senza passare id, che verrà generato casualmente.
 //  Partial: per permettere l’aggiornamento di qualsiasi proprietà tranne id e name.
+function createActress(data: Omit<Actress, "id">): Actress {
+  return { ...data, id: Math.floor(Math.random() * 1000) };
+}
 
-// function createActress(newActress: Actress): <Actress> {
-
-// return newActress;
-// }
+function updateActress(actress: Actress, update: Partial<Actress>): Actress {
+  return {
+    ...actress,
+    ...update,
+    id: actress.id,
+    name: actress.name,
+  };
+}
 
 // 🎯 BONUS 2
 // Crea un tipo Actor, che estende Person con le seguenti differenze rispetto ad Actress:
@@ -180,7 +198,122 @@ async function getActresses(ids: number[]): Promise<(Actress | null)[]> {
 //  nationality: le stesse di Actress più:
 //      Scottish, New Zealand, Hong Kong, German, Canadian, Irish.
 // Implementa anche le versioni getActor, getAllActors, getActors, createActor, updateActor.
+type Nationality_t =
+  | Nationality_p
+  | "Scottish"
+  | "New Zealand"
+  | "Honk Kong"
+  | "German"
+  | "Candian"
+  | "Irish";
+
+type Actor = Person & {
+  known_for: [string, string, string];
+  awards: [string] | [string, string];
+  nationality: Nationality_t;
+};
+
+function isActor(data: unknown): data is Actor {
+  if (
+    isPerson(data) &&
+    "known_for" in data &&
+    data.known_for instanceof Array &&
+    data.known_for.length === 3 &&
+    data.known_for.every((m) => typeof m === "string") &&
+    "awards" in data &&
+    data.awards instanceof Array &&
+    (data.awards.length === 1 || data.awards.length === 2) &&
+    data.awards.every((a) => typeof a === "string") &&
+    "nationality" in data &&
+    typeof data.nationality === "string"
+  ) {
+    return true;
+  }
+  return false;
+}
+
+async function getActor(id: number): Promise<Actor | null> {
+  try {
+    const res = await fetch(`http://localhost:5000/actors/${id}`);
+    if (!res.ok) {
+      throw new Error(`Errore ${res.status}: ${res.statusText}`);
+    }
+    const data: unknown = await res.json();
+    if (isActor(data)) {
+      return data;
+    }
+    throw new Error("Dati non validi");
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log("Errore:", error.message);
+    } else {
+      console.log("Errore sconosciuto:", error);
+    }
+    return null;
+  }
+}
+
+async function getAllActors(): Promise<Actor[]> {
+  try {
+    const res = await fetch("http://localhost:5000/actors");
+    if (!res.ok) {
+      throw new Error(`Errore ${res.status}: ${res.statusText}`);
+    }
+    const data: unknown[] = await res.json();
+    // if (Array.isArray(data) && data.every(isActor)) {
+    //   return data;
+    // }
+    const attoriPresenti = data.filter(isActor);
+    return attoriPresenti;
+
+    // return [];
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Errore nel recupero dei dati", error);
+    }
+    throw new Error("Dati non validi");
+  }
+}
+
+async function getActors(ids: number[]): Promise<(Actor | null)[]> {
+  try {
+    const promises = ids.map((id: number) => getActor(id));
+    return await Promise.all(promises);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Errore durante il recupero dei dati:", error);
+    } else {
+      console.error("Errore:", error);
+    }
+    return [];
+  }
+}
+
+function createActor(data: Omit<Actor, "id">): Actor {
+  return { ...data, id: Math.floor(Math.random() * 1000) };
+}
+
+function updateActor(actor: Actor, update: Partial<Actor>): Actor {
+  return {
+    ...actor,
+    ...update,
+    id: actor.id,
+    name: actor.name,
+  };
+}
 
 // 🎯 BONUS 3
 // Crea la funzione createRandomCouple che usa getAllActresses e getAllActors per restituire un’array che ha sempre due elementi:
 // al primo posto una Actress casuale e al secondo posto un Actor casuale.
+async function createRandomCouple(): Promise<[Actress, Actor] | null> {
+  const [actresses, actors] = await Promise.all([
+    getAllActresses(),
+    getAllActors(),
+  ]);
+  if (actresses.length === 0 || actors.length === 0) {
+    return null;
+  }
+  const randomActress = actresses[Math.floor(Math.random() * actresses.length)];
+  const randomActor = actors[Math.floor(Math.random() * actors.length)];
+  return [randomActress, randomActor];
+}
